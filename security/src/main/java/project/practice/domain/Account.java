f7258c252c2cd79c87f9fe940e88189132a8de69
Tedeeeee <gksmf35916@gmail.com>
2023-06-30 00:23:49 +0900

    mybatis 연동

--- security/src/main/java/project/practice/domain/Account.java
+++ security/src/main/java/project/practice/domain/Account.java
@@ -2,6 +2,7 @@ package project.practice.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -10,7 +11,7 @@ import lombok.Data;
 public class Account {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
     private String username;
