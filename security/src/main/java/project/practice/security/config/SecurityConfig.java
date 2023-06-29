d8c1c94ea8424296b1e72d17bd2d5d40acfecb6f
Tedeeeee <gksmf35916@gmail.com>
2023-06-29 19:01:09 +0900

    연습과 프로젝트 통합

---
+++ security/src/main/java/project/practice/security/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package project.practice.security.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // js, css, 이미지파일등 보안이 따로 필요없는 파일들을 모두 시큐리티를 그냥 통과하게 둔다.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/mypage").hasRole("USER")
+                        .requestMatchers("/messages").hasRole("MANAGER")
+                        .requestMatchers("/config").hasRole("ADMIN")
+                )
+
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public static UserDetailsService users() {
+        BCryptPasswordEncoder passwordEncoder = passwordEncoder();
+        String password = passwordEncoder.encode("1111");
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password(password)
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+}
